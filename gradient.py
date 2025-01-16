4f860f77e4e9af448514335465948f1bb587f738
khhebecpt024 <khhebecpt024@student.wethinkcode.co.za>
2025-01-16 12:05:11 +0200

    file header add to image.bmp

--- gradient.py
+++ gradient.py
@@ -1,8 +1,23 @@
 import sys 
-from struct import *
+import struct
+# from struct import *
 
-print(sys.byteorder) 
+this_endian = sys.byteorder
+print(this_endian)
 # little-endian backwards
 
-# byte size of integers 
-print(calcsize("i"))
+signature = b"BM"
+file_size = 0 #offset,w,h*3 pixel per colour = 26+100+100*3 or 
+# file_size = file_header_size + dib_header_size + pixel_data_size
+reserved1= 0
+reserved2 = 0
+data_offset = 26
+
+file_header = struct.pack("<2sIHHI", signature, file_size, reserved1, reserved2, data_offset)
+
+print(file_header)
+
+with open("image.bmp", "wb") as f:
+    f.write(file_header)
+
+print("image.bmp has file_header")
