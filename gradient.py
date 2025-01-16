1725fd3a313b38021d6e69cf986ff3f45e1e31d7
khhebecpt024 <khhebecpt024@student.wethinkcode.co.za>
2025-01-16 17:11:27 +0200

    pixel data roughdraft

--- gradient.py
+++ gradient.py
@@ -7,36 +7,46 @@ this_endian = sys.byteorder
 # little-endian backwards
 
 def create_file():
-    signature = b"BM"
-    file_size = 0 
+    signature = 19778
+    file_size = 100*100*3
     # file_size = file_header_size + dib_header_size + pixel_data_size
     reserved1= 0
     reserved2 = 0
-    data_offset = 14+40
+    data_offset = 14+12
 
-    file_header = struct.pack("<2sIHHI", signature, file_size, reserved1, reserved2, data_offset)
+    file_header = struct.pack("<LIHHI", signature, file_size, reserved1, reserved2, data_offset)
     # print(file_header)
 
     with open("image.bmp", "wb") as f:
         f.write(file_header)
 
-    header_size = 40
+    header_size = 12
     image_width = 100
     image_height = 100
     planes = 1
     bits_per_pixel = 3*8 #3bytes*8bits
-    image_size = image_width*image_height*3
-
-    dib_header = struct.pack("<IiiHHI", header_size, image_width,image_height, planes, bits_per_pixel, image_size)
+  
+    dib_header = struct.pack("<LHHHH", header_size, image_width,image_height, planes, bits_per_pixel)
     # print(dib_header)
 
     with open("image.bmp", "ab") as g:
-        g.write(dib_header)
+        g.write(dib_header) 
 
+    pixel_data = []
+    for i in range(image_width * image_height):
+        pixel_data.append(0)
+        pixel_data.append(0)
+        pixel_data.append(255)
+    # print(len(pixel_data)) #100*100*3
+    
+# pack pixel 
 
-    #  pixel 
+    with open("image.bmp", "ab") as h:
+        h.write(bytes(pixel_data))
 
-    return file_header, dib_header
+    # print(bytes(pixel_data))
+
+    return file_header, dib_header, bytes(pixel_data)
 
 def main():
     create_file()
